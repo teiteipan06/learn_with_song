7d6835fb6ce714a115f16a99129d4c1b5477fc24
teiteipan06 <y7321543@gmail.com>
2022-11-20 23:32:57 +0900

    userとsongsのmodelの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :nationality, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message:'全角（漢字・ひらがな・カタカナ）文字を使用してください' }
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/, message:'全角（漢字・ひらがな・カタカナ）文字を使用してください' }
+  validates :birthday, presence: true
+
+  has_many :songs
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "songs#index"
 
-  resources :songs, only: :index
+  resources :songs
 end
