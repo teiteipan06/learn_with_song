e9743ca1d57133d143e98ffc88332313e749390d
teiteipan06 <y7321543@gmail.com>
2022-11-29 23:38:42 +0900

    likesのcontrollerの設定

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,14 @@
+class LikesController < ApplicationController
+  
+  def create
+    @song_like = Like.new(user_id: current_user.id, song_id: params[:song_id])
+    @song_like.save
+    redirect_to song_path(params[:song_id]) 
+  end
+
+  def destroy
+    @song_like = Like.find_by(user_id: current_user.id, song_id: params[:song_id])
+    @song_like.destroy
+    redirect_to song_path(params[:song_id]) 
+  end
+end
