664bc638814c50b6155b4c2b971ea04d9e270a6e
teiteipan06 <y7321543@gmail.com>
2022-11-29 23:11:44 +0900

    マイペイージの投稿一覧の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @songs = current_user.songs
+    @songs = current_user.songs.order("created_at DESC")
   end
 end
